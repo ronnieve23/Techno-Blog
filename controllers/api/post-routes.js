8576d9d8ad89f6113a33e2dde5a3528e70e86a07
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-09-21 11:50:20 -0400

    added GET one post for post-routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -25,7 +25,34 @@ router.get ('/', (req, res) => {
     });
 });
 
-
+router.get ('/:id', (req, res) => {
+    console.log('Grabbing Posts');
+    Post.findOne({
+       attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+    ],
+    include: [
+        {
+            model: User,
+            attributes:['username'] 
+        }
+    ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 
 module.exports = router;
