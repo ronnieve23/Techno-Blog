9ed2069095a1128fea48b5e2e7449e1af76cd90c
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-09-21 12:57:11 -0400

    added PUT and DELETE routes to post-routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -67,31 +67,52 @@ router.post('/', (req, res) => {
         });
 });
 
-// router.put('/:id', (req, res) => {
-//     console.log('Grabbing Posts');
-//     Post.update(
-//         {
-//             title: req.body.title
-//         },
-//         {
-//             where :
-//             {
-//                 id: req.params.id
-//             }
-//         }
-//     )
-//         .then(dbPostData => {
-//             if (!dbPostData) {
-//                 res.status(404).json({ message: 'No post found with this id' });
-//                 return;
-//             }
-//             res.json(dbPostData);
-//         })
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         });
-// });
+router.put('/:id',(req, res) => {
+    Post.update(
+      {
+        title: req.body.title,
+        post_body:req.body.post_body
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  router.delete('/:id',(req, res) => {
+    Post.destroy(
+      
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 
 module.exports = router;
