4cfbcfd0bdf09e7d93d743331a5191ccf6aff948
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-09-21 13:14:57 -0400

    finished comment routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection')
-const { Post, User } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 router.get('/', (req, res) => {
     console.log('Grabbing Posts');
@@ -12,6 +12,14 @@ router.get('/', (req, res) => {
             'created_at',
         ],
         include: [
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+        },
             {
                 model: User,
                 attributes: ['username']
