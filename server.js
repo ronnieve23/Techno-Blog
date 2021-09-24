edabb8549658ab6bd9debcea0bbe8cde67947f4e
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-09-24 19:31:50 -0400

    started work on handlebars

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -25,6 +26,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use (routes);
 
