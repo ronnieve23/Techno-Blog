7fa3bc83ab93cf995a5d4d3ac96984b78432ede7
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-09-26 08:20:03 -0400

    added helpers for dates

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({helpers});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
