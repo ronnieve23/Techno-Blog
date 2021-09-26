4b83087239493878285ab923653fc7021829c060
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-09-26 08:51:32 -0400

    added middleware

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection')
 const { Post, User, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
 
 router.get('/', (req, res) => {
   console.log('Grabbing Posts');
@@ -72,7 +74,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
   Post.create({
     title: req.body.title,
     post_body: req.body.post_body,
@@ -85,7 +87,7 @@ router.post('/', (req, res) => {
     });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',withAuth, (req, res) => {
   Post.update(
     {
       title: req.body.title,
@@ -110,7 +112,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',withAuth, (req, res) => {
   Post.destroy(
 
     {
